242b709df08dd2abf0f9054d3329251d32a26657
Jordan Ribera <jordanribera@gmail.com>
2015-09-22 12:05:19 -0700

    starting things up

---
+++ comb.py
@@ -0,0 +1,17 @@
+import boto3
+#from boto3.session import session
+
+session = boto3.session.Session(aws_access_key_id='AKIAITENMIYRT6XQBQJA', aws_secret_access_key='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', region_name='us-west-2')
+
+print("#### displaying buckets ####")
+
+s3 = session.resource('s3');
+for bucket in s3.buckets.all():
+	print(bucket.name);
+
+print('')
+print('#### displaying contents ####')
+
+bucket = s3.Bucket('s3-comb-test')
+for object in bucket.objects.all():
+	print(object.key)
